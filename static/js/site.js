5074b643a6841e308a6fc9accefa8eef396d0bbe
brkcmd <brkcmd@users.noreply.github.com>
2018-04-16 16:01:06 -0400

    Improvements to YouTube integration.

--- static/js/site.js
+++ static/js/site.js
@@ -4,25 +4,28 @@
         var div, n,
             v = document.querySelectorAll(".youtube-player");
         for (n = 0; n < v.length; n++) {
-            div = document.createElement("div");
-            div.setAttribute("data-id", v[n].dataset.id);
-            div.innerHTML = ytThumb(v[n].dataset.id);
-            div.addEventListener("click", ytIframe);
+            a = document.createElement("a");
+            a.setAttribute("data-id", v[n].dataset.id);
+            a.setAttribute("href", "https://www.youtube.com/watch?v=" + v[n].dataset.id);
+            a.innerHTML = ytThumb(v[n].dataset.id);
+            a.addEventListener("click", ytIframe);
             v[n].classList.add("js-enabled");
-            v[n].appendChild(div);
+            v[n].appendChild(a);
         }
     });
 
     function ytThumb(id) {
         var thumb = '<img src="/thumbs/{}.jpg">',
-            play = '<i class="material-icons play">&#xe037;</i>';
-        return thumb.replace("{}", id) + play;
+            play = '<i class="material-icons play">&#xe037;</i>',
+            privacyNotice = '<div class="privacy">Note: loading this video will request resources from YouTube.</div>';
+        return thumb.replace("{}", id) + play + privacyNotice;
     }
 
-    function ytIframe() {
+    function ytIframe(e) {
         var iframe = document.createElement("iframe");
         var embed = "https://www.youtube-nocookie.com/embed/{}?rel=0&autoplay=1";
         iframe.setAttribute("src", embed.replace("{}", this.dataset.id));
         this.parentNode.replaceChild(iframe, this);
+        e.preventDefault();
     }
 })();
